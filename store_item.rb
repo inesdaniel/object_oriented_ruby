9cd5ef7aefea605a32da067c72abd86c2c752c9c
Ines Daniel <i.danie8@gmail.com>
2018-06-19 18:58:35 -0500

    added input_options and hashes

--- store_item.rb
+++ store_item.rb
@@ -1,17 +1,12 @@
-# item1 = {"name" => "shirt", "color" => "blue", "price" => 10}
-# item2 = {"name" => "pants", "color" => "black", "price" => 20}
-# item3 = {:name => "shoes", :color => "orange", :price => 30}
-
-# p " A #{item1["color"]} #{item1["name"]} costs $#{item1["price"]}"
 
 class Clothing
   attr_reader :name, :color, :price
   attr_writer :price
 
-  def initialize(name, color, price)
-    @name = name
-    @color = color
-    @price = price
+  def initialize(input_options)
+    @name = input_options[:name]
+    @color = input_options[:color]
+    @price = input_options[:price]
   end
 
   def print_info
@@ -20,9 +15,9 @@ class Clothing
 
 end
 
-item1 = Clothing.new("shirt", "blue", 10)
-item2 = Clothing.new("pants", "black", 20)
-item3 = Clothing.new("shoes", "orange", 30)
+item1 = Clothing.new(name: "shirt", color: "blue", price: 10)
+item2 = Clothing.new({:name => "pants", :color => "black", :price => 20})
+item3 = Clothing.new(name: "shoes", color: "orange", price: 30)
 
 item1.price= 20
 p item1.print_info
