4520bfd22cd0d6c0364f5ae7c7a08852a59b60e0
Ines Daniel <i.danie8@gmail.com>
2018-06-18 20:33:15 -0500

    initial store file

---
+++ store_item.rb
@@ -0,0 +1,5 @@
+item1 = {"name" => "shirt", "color" => "blue", "price" => 10}
+item2 = {"name" => "pants", "color" => "black", "price" => 20}
+item3 = {:name => "shoes", :color => "orange", :price => 30}
+
+p " A #{item1["color"]} #{item1["name"]} costs $#{item1["price"]}"
